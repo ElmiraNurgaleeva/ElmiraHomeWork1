a2eb7e96453d1f9dc14d46976276f1ab4b5a69ee
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-04-29 23:48:39 +0300

    Решаем задачу3 на семинаре

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Написать программу, которая будет выдавать название дня недели
+// по заданному номеру, 3 -> Среда   5 -> Пятница
+
+
 Console.WriteLine("Hello, World!");
