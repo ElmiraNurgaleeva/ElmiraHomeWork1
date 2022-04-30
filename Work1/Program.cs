47a4a13c7ea5116aa5dd9c6e56243ffee60666d5
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-04-30 23:46:32 +0300

    Work1 Решена задача1 к семинару1

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+
+Console.Write("Введите число 1 ->  ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 ->  ");
+int num2 = int.Parse(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+
+int min = num2;
+if (num1 < min) min = num1;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
+Console.Write("min= ");
+Console.WriteLine(min);
