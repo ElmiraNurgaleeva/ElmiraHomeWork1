6a44b36ddf74f4c904f6b6cac619cc3723a7d486
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-04-30 23:58:25 +0300

    Work2 Решена задача2 к семинару 1

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число 1 ->  ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 ->  ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3 ->  ");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max= ");
+Console.WriteLine(max);
