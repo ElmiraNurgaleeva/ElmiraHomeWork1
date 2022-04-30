c36eb8512fdc4d8fec4b38d072e286b1b1ac68c0
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-04-30 23:59:30 +0300

    Добавлены4

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает одно число N, 
+//а на выходе показывает все целые числа в промежутке от -N до N
+//Напрмер, 4 ->  "-4 -3 -2 -1 0 1 2 3 4"
+
 Console.WriteLine("Hello, World!");
