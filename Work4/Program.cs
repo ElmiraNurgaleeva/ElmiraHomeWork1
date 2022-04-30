5b5df7654afccf310c86d4721b53b3d97d0ffe62
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-01 00:17:52 +0300

    Work4 Решена задача4 к семинару1

--- Work4/Program.cs
+++ Work4/Program.cs
@@ -2,4 +2,16 @@
 // принимает число (N), а на выходе показывает все чётные
 // числа от 1 до N.
 
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int start = 0;
+while (start <= num)
+{
 
+        if (start % 2 == 0)
+{
+        Console.Write(start+", ");
+
+}
+    start++;
+    }
