b36feddd30e8e30bb44a0cf5374bc6af72010f94
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-04-30 21:07:18 +0300

    Решение задачи2 на семинаре

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,6 +1,19 @@
 ﻿// Напишите программу, которая на вход принимает 2 числа и проверяет, 
 //является ли 1е квадратом второго, например, а=25, в =5 -> да
 
-
-
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число a:");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число b:");
+int num2 = int.Parse(Console.ReadLine());
+//Console.Write("Квадрат числа равен :");
+int num.Result = num1 * num1";
+if (numResult == num2),
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
+//Console.WriteLine(numResult);
+Console.ReadKey();
