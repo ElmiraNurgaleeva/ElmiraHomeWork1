dd6735523126b5a29fbbb73bf84324e2857a8007
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-02 23:42:26 +0300

    Решены 5 задач на Лекции1

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задачи на лекции№1   Задачи №1 и №2
+
+//Console.WriteLine("Введите ваше имя ");
+//string usrname = Console.ReadLine();
+//Console.WriteLine("Привет,  ");
+//Console.WriteLine(usrname);
+
+//Console.Write("Введите ваше имя ");
+//string usrname = Console.ReadLine();
+//Console.Write("Привет,  ");
+//Console.Write(usrname);
+
+// Задача№3  : Написать программму сложения 2х чисел  !!!Провести все УТОЧНЕНИЯ для написания программы
+
+//Console.WriteLine("Ответ ");
+//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+//Еще вариант через промежуточную переменную -  эта же задача:
+
+//Console.WriteLine("Ответ ");
+//int numberA = 31;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+//Задача4  : Написать программу на языке С#, в которую пользователь вводит 2 целых числа и получает частное введенных числе
+//здесь исп только ЦЕЛЫЕ ЧИСЛА!!!
+
+//Console.WriteLine("Ответ ");
+//int numberA = 10;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+
+//Таже задача + новый тип данных чтобы можно было исп не только целые числа, а с вещественными числами
+
+//Console.WriteLine("Ответ ");
+//double numberA = 12.8;
+//double numberB = 5.2;
+//Console.WriteLine(numberA / numberB);
+
+// Задача №5  : Случайное целое цисло
+
+//Console.WriteLine("Ответ ");
+//int numberA = new Random().Next(1, 10);   // 1 2 3 4 5 ... макс числом будет 9
+//int numberB = new Random().Next(1, 10);
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+//Таже задача и чтобы не гадать какие случайные числа берет программа  :
+
+//Console.WriteLine("Ответ ");
+//int numberA = new Random().Next(1, 10);   // 1 2 3 4 5 ... макс числом будет 9
+//Console.WriteLine(numberA);
+//int numberB = new Random().Next(1, 10);
+//Console.WriteLine(numberB);
+//int result = numberA + numberB;
+// Console.WriteLine(result);
