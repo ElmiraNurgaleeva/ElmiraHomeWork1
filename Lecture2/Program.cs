5a18831edc76db941bd40555239203359f5d6ed2
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-03 21:50:01 +0300

    Решены задачи все

---
+++ Lecture2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишем программу, которая будет реагировать на каждого по особеннму исп If  Else :Э
+
+Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
