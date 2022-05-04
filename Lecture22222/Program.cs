1ba8d0281c64fcc65b21eeed6412f55053c3cd87
Nurgaleeva Elmira <Elmira8013@yandex.ru>
2022-05-04 21:09:52 +0300

    Добавлена Лекция22222

---
+++ Lecture22222/Program.cs
@@ -0,0 +1,30 @@
+﻿// Метод или функция которая будет выводить все элементы по порядку - Код поиска превратив в Метод :
+//Есть методы в языке СиШарп которые могут возвращать значения а могeт не возвращать, метод не возвращает  это Void
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;   // или  index = index + 1
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10];   // new int [10]; - означает создай новый массив в котором будет 10 элементов, по умолч здесь будут 000
+
+FillArray(array);     // Метод заполнения массива
+PrintArray(array);    //Метод печати массива
+
